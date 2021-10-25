164b8daf3af8870d9c7e57b79a7e550c159ca0a7
xiusin <826466266@qq.com>
2021-10-25 16:09:32 +0800

    添加Entity, 支持链式

--- entity.go
+++ entity.go
@@ -8,7 +8,7 @@ type LogEntity struct {
 	sync.Mutex
 }
 
-func (entity *LogEntity) SetDataKV(key string, val interface{}) {
+func (entity *LogEntity) SetDataKV(key string, val interface{}) *LogEntity {
 	entity.Lock()
 	defer entity.Unlock()
 
@@ -16,9 +16,10 @@ func (entity *LogEntity) SetDataKV(key string, val interface{}) {
 		entity.entityData = map[string]interface{}{}
 	}
 	entity.entityData[key] = val
+	return entity
 }
 
-func (entity *LogEntity) SetData(data map[string]interface{}) {
+func (entity *LogEntity) SetData(data map[string]interface{}) *LogEntity {
 	entity.Lock()
 	defer entity.Unlock()
 	if entity.entityData == nil {
@@ -28,6 +29,7 @@ func (entity *LogEntity) SetData(data map[string]interface{}) {
 	for k, v := range data {
 		entity.entityData[k] = v
 	}
+	return entity
 }
 
 func (entity *LogEntity) getDatas() map[string]interface{} {
